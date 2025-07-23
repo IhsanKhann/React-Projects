4ff1199f110606d748c5ad5d133221cc39dc762e
Ihsan <mortalihsan@gmail.com>
2025-07-23 09:37:45 +0500

    Added styling to the header

--- InstagramMini/src/components/Header/Header.jsx
+++ InstagramMini/src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
 import './Header.css';
 import { NavLink } from 'react-router-dom';
 import { FiHome, FiSend, FiCompass, FiHeart } from 'react-icons/fi';
+import { useContext } from 'react';
+import { ThemeContext } from '../Context/ThemeContext';
 
 function Header() {
-  return (
-    <header className="header">
-      <div className="logo">MyGram</div>
+  const {Theme} = useContext(ThemeContext) ;
 
-      <div className="search-bar">
+  return (
+     <header className={`header ${Theme}`}>
+      <div className={`logo ${Theme}`}>MyGram</div>
+
+      <div className={`search-bar ${Theme}`}>
         <input type="text" placeholder="Search" />
       </div>
 
-      <nav className="nav-icons">
+      <nav className={`nav-icons ${Theme}`}>
         <NavLink to="/home" className="icon"><FiHome /></NavLink>
         <NavLink to="/messages" className="icon"><FiSend /></NavLink>
         <NavLink to="/explore" className="icon"><FiCompass /></NavLink>
         <NavLink to="/notifications" className="icon"><FiHeart /></NavLink>
 
-        <NavLink to="/profile" className="profile-pic">
+        <NavLink to="/profile" className={`profile-pic ${Theme}`}>
           <img src="https://i.pravatar.cc/150?img=8" alt="Profile" />
         </NavLink>
       </nav>
